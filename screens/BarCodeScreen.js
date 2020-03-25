98110bc0c6f2dadb6a01c232b014f842852bd14c
rowsharif <57699453+rowsharif@users.noreply.github.com>
2020-03-25 21:08:28 +0300

    v3

--- screens/BarCodeScreen.js
+++ screens/BarCodeScreen.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import { ScrollView, StyleSheet } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import React, { useState, useEffect } from "react";
+import { Text, View, Button } from "react-native";
 
 export default function BarCodeScreen() {
   const [hasPermission, setHasPermission] = useState(null);
@@ -16,16 +17,17 @@ export default function BarCodeScreen() {
   const handleBarCodeScanned = async ({ type, data }) => {
     setScanned(true);
     //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-    const response = await fetch(`https://api.barcodelookup.com/v2/products?barcode=${data}&formatted=y&key=r7w70x27axuy3iveqikreebx0061ww`)
-    const json = await response.json()
-    console.log('json', json)
+    const response = await fetch(
+      `https://api.barcodelookup.com/v2/products?barcode=${data}&formatted=y&key=22j05mzltfwjeijxedkeozr45ngee6`
+    );
+    const json = await response.json();
+
     alert(`Product is ${json.products[0].product_name}`);
   };
 
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
   }
-
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
