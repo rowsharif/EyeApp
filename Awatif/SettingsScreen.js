cb34309fb2f59e087e3dbe456b74efbfa5e2cadf
awatif97 <61411840+awatif97@users.noreply.github.com>
2020-04-02 21:32:27 +0300

    new

---
+++ Awatif/SettingsScreen.js
@@ -0,0 +1,171 @@
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import * as Speech from 'expo-speech';
+
+import React, { useState, useEffect } from "react";
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationEvents } from "react-navigation";
+
+import { Camera } from "expo-camera";
+import * as FaceDetector from "expo-face-detector";
+import * as Permissions from "expo-permissions";
+import * as ImageManipulator from "expo-image-manipulator";
+
+import Clarifai from "clarifai";
+
+const app = new Clarifai.App({
+  apiKey: "14ed2164e0c04fb3946ea51b5748aa53"
+});
+process.nextTick = setImmediate;
+
+const resize = async uri => {
+  let manipulatedImage = await ImageManipulator.manipulateAsync(
+    uri,
+    [{ resize: { height: 300, width: 300 } }],
+    { base64: true }
+  );
+  return manipulatedImage.base64;
+};
+
+const predict = async base64 => {
+  const response = await app.models.predict(Clarifai.GENERAL_MODEL, { base64 });
+  console.log("predict result", response);
+  return response;
+};
+
+export default function SettingsScreen(props) {
+  const [predictions, setPredictions] = useState([{ name: "hi" }]);
+  const [loaded, setLoaded] = useState(true);
+
+  const [hasCameraPermission, setHasCameraPermission] = useState(false);
+  const askPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    setHasCameraPermission(status === "granted");
+  };
+
+  useEffect(() => {
+    Speech.speak("Surrounding screen")
+
+    askPermission();
+  }, []);
+  useEffect(() => {
+    console.log("sdfghjmk,l.ghjkl;/");
+  }, []);
+
+  const capturePhoto = async () => {
+    const photo = await this.camera.takePictureAsync();
+    console.log("uri of photo capture", photo.uri);
+    return photo.uri;
+  };
+
+  //method call when Capture Image button pressed
+  //set the state for predictions get
+
+  const objectDetection = async () => {
+    const photo = await capturePhoto();
+    const resized = await resize(photo);
+    const predictions = await predict(resized);
+    setPredictions(predictions.outputs[0].data.concepts);
+    console.log("predictions");
+    console.log(predictions);
+  };
+  const check = () => {
+    console.log("sdgvdsfgdsfdbrgs");
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <NavigationEvents
+        onWillFocus={payload => setLoaded(true)}
+        onDidBlur={payload => setLoaded(false)}
+      />
+      {loaded && (
+        <Camera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={{ flex: 1 }}
+          type={Camera.Constants.Type.back}
+        >
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: "transparent",
+              flexDirection: "row"
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                flex: 1,
+                alignSelf: "flex-end",
+                alignItems: "center",
+                backgroundColor: "black"
+              }}
+              onPress={objectDetection}
+            >
+              <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+                Capture Image
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                flex: 1,
+                alignSelf: "flex-end",
+                alignItems: "center",
+                backgroundColor: "black"
+              }}
+              onPress={check}
+            >
+              <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+                {predictions[0].name}
+                {Speech.speak(predictions[0].name)}
+
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      )}
+    </View>
+  );
+}
+async function loadResourcesAsync() {
+  await Promise.all([
+    Asset.loadAsync([
+      require("../assets/images/robot-dev.png"),
+      require("../assets/images/robot-prod.png")
+    ]),
+    Font.loadAsync({
+      // This is the font that we are using for our tab bar
+      ...Ionicons.font,
+      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
+      // remove this if you are not using it in your app
+      "space-mono": require("../assets/fonts/SpaceMono-Regular.ttf")
+    })
+  ]);
+}
+
+function handleLoadingError(error) {
+  // In this case, you might want to report the error to your error reporting
+  // service, for example Sentry
+  console.warn(error);
+}
+
+function handleFinishLoading(setLoadingComplete) {
+  setLoadingComplete(true);
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  }
+});
--- Awatif/StepScreen.js
+++ Awatif/StepScreen.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from "react";
 import { Pedometer } from "expo-sensors";
 import { StyleSheet, Text, View,TouchableOpacity } from "react-native";
+import * as Speech from 'expo-speech';
 
 export default function StepScreen() {
     const [isPedometerAvailable,setisPedometerAvailable]=useState("checking");
@@ -15,9 +16,7 @@ export default function StepScreen() {
             Pedometer.isAvailableAsync().then(
             result => {
                 setisPedometerAvailable(String(result))
-                Speech.speak(setisPedometerAvailable(String(result)))
-
-             },
+            },
             error => {
                 setisPedometerAvailable("Could not get isPedometerAvailable: " + error)
                 }
@@ -28,6 +27,8 @@ export default function StepScreen() {
             Pedometer.getStepCountAsync(start, end).then(
             result => {
                 setpastStepCount(result.steps )
+                Speech.speak(result.steps)
+
             },
             error => {
                 setpastStepCount("Could not get stepCount: " + error)
@@ -40,6 +41,8 @@ export default function StepScreen() {
     _subscription = null;
   }; 
   useEffect(() => {
+    Speech.speak("Step screen.")
+
             _subscribe();
   },[])
 
@@ -57,12 +60,15 @@ export default function StepScreen() {
       <View style={styles.container}>
         <Text>
           Steps taken in the last 24 hours: {pastStepCount}
+
         </Text>
           <TouchableOpacity
         style={styles.button}
        
       >
-        <Text>Steps you have taken: {currentStepCount}</Text>
+        <Text>Steps you have taken: {currentStepCount}
+        {Speech.speak("Steps you have taken:".concat(currentStepCount.toString()))}
+</Text>
       </TouchableOpacity>       
       <TouchableOpacity
         style={styles.button}
